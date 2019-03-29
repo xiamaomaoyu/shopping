fa1e2cb9df4e2ee3e06652a85ed6b7408e30f260
xiamaomaoyu <summerli1997@gmail.com>
2019-03-30 07:45:38 +1100

    Frontend: add chat,item,orders,user

--- shopping.py
+++ shopping.py
@@ -8,5 +8,26 @@ def hello_world():
     return render_template("homepage.html")
 
 
+@app.route('/cart')
+def mycart():
+    return render_template("cart.html")
+
+@app.route('/chat')
+def mychat():
+    return render_template("chat.html")
+
+@app.route('/item')
+def item():
+    return render_template("item.html")
+
+@app.route('/user')
+def user():
+    return render_template("user.html")
+
+@app.route('/orders')
+def orders():
+    return render_template("orders.html")
+
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(port=5000, host='0.0.0.0',debug=True)
