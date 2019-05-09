8b14ff486355c5ab46f43b11f18f590c2276afac
haokongzhang <haokongzhang@gmail.com>
2019-05-10 03:04:59 +1000

    finish deletion

--- shopping.py
+++ shopping.py
@@ -57,4 +57,4 @@ def search(keyword=None):
     return render_template("search.html",keyword=keyword)
 
 if __name__ == '__main__':
-    app.run(debug=True,port=5000)
+    app.run(host='0.0.0.0',port=80)
