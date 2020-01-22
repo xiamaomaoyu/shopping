1dcd7472837ae6fa235210525ad86caea4bac708
Summer <summerli1997@gmail.com>
2020-01-22 10:33:10 +0000

    Updated new db

--- shopping.py
+++ shopping.py
@@ -285,5 +285,5 @@ def handle_user_leave(str, methods=['GET', 'POST']):
 
 
 if __name__ == '__main__':
-    socketio.run(app, port=80, host='0.0.0.0', debug=True)
+    socketio.run(app, port=80, host='0.0.0.0')
 
